a6add9732d349565068c034caae0046517467618
luizdivino <nandodivinodev@gmail.com>
2026-03-01 02:50:16 -0300

    adicionei a configuração do dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
-import 'reflect-metadata';
+import "reflect-metadata";
 import { appDataSource } from "./database/data-source";
 import routes from "./routes";
+import { configDotenv } from "dotenv";
+configDotenv();
 
-appDataSource.initialize().then(()=>{
-    const server = express()
-    server.use(express.json())
-    server.use(routes)
-    return server.listen(process.env.PORT, ()=>{
-        console.log(`Server running`)
-    })
-})
+
+appDataSource.initialize().then(() => {
+  const server = express();
+  server.use(express.json());
+  server.use(routes);
+  return server.listen(process.env.PORT, () => {
+    console.log(`Server running ${process.env.PORT}`);
+  });
+});
