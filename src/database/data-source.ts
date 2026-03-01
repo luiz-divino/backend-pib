473e88f25d32f9415fd0e4cfa20f6134948c0701
luizdivino <nandodivinodev@gmail.com>
2026-03-01 15:08:22 -0300

    reformulei o codigo para realização do deploy do banco de dados e fiz a conexão com um novo banco: postgresql

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,10 +1,22 @@
 import { DataSource } from "typeorm";
-import { User } from "../entities/User";
+import { configDotenv } from "dotenv";
+configDotenv();
+
+const dbport = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432;
 
 export const appDataSource = new DataSource({
-  type: "sqlite",
-  database: "./src/database/database.sqlite",
-  entities: [User],
-  migrations: ['./src/database/migrations/*.{ts,js}']
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: dbport,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
+  synchronize: true,
+  logging: true,
+  entities: ["./src/entities/*.{ts,js}"],
+  migrations: ["./src/database/migrations/*.{ts,js}"],
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
